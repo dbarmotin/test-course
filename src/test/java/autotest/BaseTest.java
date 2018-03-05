59b5577ad045114ecfe313c783c4fd5add8162ac
barmotin <barmotin@haulmont.com>
2018-03-05 22:52:35 +0300

    Конфиги, часть первая

--- src/test/java/autotest/BaseTest.java
+++ src/test/java/autotest/BaseTest.java
@@ -2,18 +2,31 @@ package autotest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ReporterConfig;
+import org.testng.ReporterConfig.Property;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected static WebDriver driver;
-
+    protected static String driverHome;
     @BeforeSuite
-    public void DriverStart(){
+    public void DriverStart() throws IOException {
+        Properties property = new Properties();
+        InputStream  is = new FileInputStream("src/main/resources/aplication.properties");
+        property.load(is);
+        driverHome = property.getProperty(driverHome);
        // System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
-        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+       // System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver",driverHome);
+
 
 
         driver = new ChromeDriver();
